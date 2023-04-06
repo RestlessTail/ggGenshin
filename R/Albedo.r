d54c7f79624666fee16f68898e50d064130c2576
restlesstail <1826930551@qq.com>
2023-04-06 23:19:13 +0800

    update .gitignore. Albedo.r: replace '=' with '<-'.

--- R/Albedo.r
+++ R/Albedo.r
@@ -15,7 +15,7 @@
 #' pal_albedo(alpha = 0.5)(10)
 #' pal_albedo(alpha = 0.5, reverse = TRUE)(20)
 pal_albedo = function(alpha = 1, reverse = FALSE) {
-	pal = c("#282532", "#3A3A68", "#E5D8CB", "#F5CE8A", "#8E6756")
+	pal <- c("#282532", "#3A3A68", "#E5D8CB", "#F5CE8A", "#8E6756")
 	pal_rgb = col2rgb(pal)
 	return(function(x){
 		vec = 1:x
